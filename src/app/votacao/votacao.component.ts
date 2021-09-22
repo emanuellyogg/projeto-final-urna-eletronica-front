57ed9442b8cb8cc20bbf4f62eaa2ccd6a35bf228
Bruna Neto Bitencourt <bruna.bitencourt@capgemini.com>
2021-09-22 15:45:06 -0300

    Criação da requisição GET config para o backend

--- src/app/votacao/votacao.component.ts
+++ src/app/votacao/votacao.component.ts
@@ -1,4 +1,8 @@
+import { Candidato } from './../../../models/candidato.model';
+import { VotacaoService } from './../votacaoServ/votacao.service';
+import { Config } from './../../../models/config.model';
 import { Component, OnInit } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-votacao',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votacao.component.css']
 })
 export class VotacaoComponent implements OnInit {
+  config: any = []
+  candidatos: Candidato[] = []
 
-  constructor() { }
+  constructor(private service: VotacaoService) { }
 
   ngOnInit(): void {
+    this.service.getConfig().subscribe((configServer: Config) => {
+      console.log(configServer);
+      this.config = configServer
+      this.candidatos = this.config.resp.candidatos
+      this.criarDataList()
+      if(this.config.resp.ehAnonima){
+        this.escondeCampoCPF()
+      }else{
+        this.colocaPlaceHolderNoCPF()
+      }
+    })
+  }
+
+  public criarDataList(){
+
+  }
+
+  public escondeCampoCPF(){
+
+  }
+
+  public colocaPlaceHolderNoCPF(){
+
   }
 
 }
