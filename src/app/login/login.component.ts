e74575e8c9f1f3897c35d3e90ef4e5b1d019f124
Larissa Henrique <larissa.henrique@capgemini.com>
2021-09-22 18:34:54 -0300

    feat: exibido periodo de votacao no login conforme arquivo config

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,6 +11,8 @@ export class LoginComponent implements OnInit {
   public user: any;
   public inputUser: string = ""
   public config: any = []
+  public inicioVotacao: any 
+  public finalVotacao: any
 
   constructor(private service: LoginService) {}
 
@@ -18,9 +20,17 @@ export class LoginComponent implements OnInit {
     
     this.service.getConfig().subscribe((configServer: Config) => {
       this.config = configServer
+
       console.log(this.config);
-      console.log(this.config.resp.inicioVotacao);
+      console.log(typeof(this.config.resp.inicioVotacao));
       console.log(this.config.resp.finalVotacao);
+
+      this.inicioVotacao = new Date(this.config.resp.inicioVotacao) 
+      this.finalVotacao = new Date(this.config.resp.finalVotacao) 
+
+      console.log(this.inicioVotacao);
+      console.log(this.finalVotacao);
+      
 
     })
   }
