4e6c140404d119adfd50f214000c01aebdb35de0
Bruna Neto Bitencourt <bruna.bitencourt@capgemini.com>
2021-09-22 15:25:25 -0300

    Criação da função getConfig no serviceVotacao

--- src/app/votacaoServ/votacao.service.ts
+++ src/app/votacaoServ/votacao.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { Config } from 'models/config.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VotacaoService {
+  private urlGet = "http://localhost:3001/config"
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  public getConfig(): Observable<Config>{
+    return this.httpClient.get<Config>(this.urlGet)
+  }
 }
