ba00a912e76573b7b5b2a52ffc7e48b209c03a23
Emanuelly Maria Ogg Vicente <emanuelly.vicente@capgemini.com>
2021-09-23 10:40:47 -0300

    feat: lógica de validação de usuário

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Config } from 'models/config.model';
-import { LoginService } from '../service login/login.service';
+import { LoginService } from '../loginServ/login.service';
 
 @Component({
   selector: 'app-login',
@@ -9,48 +9,68 @@ import { LoginService } from '../service login/login.service';
 })
 export class LoginComponent implements OnInit {
   public user: any;
-  public inputUser: string = ""
-  public config: any = []
-  public inicioVotacao: any 
-  public finalVotacao: any
-  public horaAtual: any
-  public horaValida: any
+  public inputUser: string = '';
+  public config: any = [];
+  public inicioVotacao: any;
+  public finalVotacao: any;
+  public horaAtual: any;
+  public horaValida: any;
 
   constructor(private service: LoginService) {}
 
   ngOnInit(): void {
-      this.service.getConfig().subscribe((configServer: Config) => {
-      this.config = configServer
+    this.service.getConfig().subscribe((configServer: Config) => {
+      this.config = configServer;
 
-      this.inicioVotacao = new Date(this.config.resp.inicioVotacao) 
-      this.finalVotacao = new Date(this.config.resp.finalVotacao) 
+      this.inicioVotacao = new Date(this.config.resp.inicioVotacao);
+      this.finalVotacao = new Date(this.config.resp.finalVotacao);
 
-      this.validarHorarioVotacao()
-    })
+      this.validarHorarioVotacao();
+    });
   }
 
   validarHorarioVotacao() {
-    this.horaAtual = new Date()
+    this.horaAtual = new Date();
 
-    if (this.horaAtual > this.inicioVotacao && this.horaAtual < this.finalVotacao){
-      this.horaValida = 'valida'      
+    if (
+      this.horaAtual > this.inicioVotacao &&
+      this.horaAtual < this.finalVotacao
+    ) {
+      this.horaValida = 'valida';
     } else {
-      if (this.horaAtual < this.inicioVotacao){
-        this.horaValida = 'naoIniciada'
+      if (this.horaAtual < this.inicioVotacao) {
+        this.horaValida = 'naoIniciada';
       }
-      if (this.horaAtual > this.finalVotacao){
-        this.horaValida = 'finalizada'
+      if (this.horaAtual > this.finalVotacao) {
+        this.horaValida = 'finalizada';
       }
     }
   }
 
   logar() {
-    const user = {nmUser: this.inputUser}
+    const user = { nmUser: this.inputUser };
 
     this.service.getUser(user).subscribe((userServer) => {
       console.log(userServer.id);
-      this.user = userServer.id
-    })
+      this.user = userServer.id;
+
+      console.log(user);
+
+      this.validarUser()
+
+    });
+
+  }
+
+  validarUser() {
+
+    if (this.user == 'invalido') {
+      console.log('Usuário inválido');
+
+      return;
+    }
+
+    console.log("passou aqui");
 
   }
 }
