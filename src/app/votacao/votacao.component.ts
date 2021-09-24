1cdb7f0f08dc5d679a196d02d5f062c23d0e0c52
sofia piantavini <sofia.piantavini@capgemini.com>
2021-09-24 11:03:31 -0300

    correção do bug no git

--- src/app/votacao/votacao.component.ts
+++ src/app/votacao/votacao.component.ts
@@ -151,8 +151,7 @@ export class VotacaoComponent implements OnInit {
   public limpaSelect(){
 
     this.candSelect = ""
-    this.atualizaCandSelecionado() 
+    this.atualizaCandSelecionado()
   }
-
-
+  
 }
